d1f11da6a8ba5b23e00eee8591841b29750c55dc
Benjamin Kay <github@bkay.uk>
2024-03-09 17:41:13 +0000

    We have a todo list

--- routes/todo.js
+++ routes/todo.js
@@ -1,20 +1,21 @@
 var express = require('express');
 const { handlebars } = require('hbs');
 var router = express.Router();
+const { v4 : uuid } = require('uuid');
 
 const todoList = [
     {
-        id: 0,
+        id: 'test-1',
         todo: 'Decide to get this out of local memory',
         complete: false
     },
     {
-        id: 1,
+        id: 'test-2',
         todo: 'Make proper database',
         complete: false
     },
     {
-        id: 2,
+        id: 'test-3',
         todo: 'Move this to websockets as per original plan',
         complete: false
     },
@@ -22,15 +23,56 @@ const todoList = [
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('todo', { title: 'Todo\'s', todoList });
+    res.render('todo', { title: 'Todo\'s', todoList });
 });
 
 router.post('/add', function(req, res, next) {
-    console.log(req.body)
     if(req.body.new_todo) {
-        todoList.push({id: todoList.length, todo: req.body.new_todo, complete: false})
+        todoList.push({id: uuid(), todo: req.body.new_todo, complete: false})
     }
     res.render('partials/todo-list', { todoList, layout: false });
-  });
+});
+
+router.patch('/complete/:id', function(req, res, next) {
+    const index = todoList.findIndex(({id}) => id == req.params.id)
+    if (index === -1) return res.statusCode = 404
+
+    const newItem = {
+        ...todoList[index],
+        complete: !todoList[index].complete,
+    }
+
+    todoList[index] = newItem
+    res.render('partials/todo-item', { ...newItem, layout: false });
+});
+
+router.get('/edit/:id', function(req, res, next) {
+    const index = todoList.findIndex(({id}) => id == req.params.id)
+    if (index === -1) return res.statusCode = 404
+
+    res.render('partials/todo-item-editable', { ...todoList[index], layout: false });
+});
+
+router.patch('/edit/:id', function(req, res, next) {
+    const index = todoList.findIndex(({id}) => id == req.params.id)
+    if (index === -1) return res.statusCode = 404
+
+    const newItem = {
+        ...todoList[index],
+        todo: req.body.edit_todo
+    }
+
+    todoList[index] = newItem
+    res.render('partials/todo-item', { ...newItem, layout: false });
+});
+
+router.delete('/delete/:id', function(req, res, next) {
+    const index = todoList.findIndex(({id}) => id == req.params.id)
+    if (index === -1) return res.statusCode(404)
+    
+    todoList.splice(index, 1)
+
+    res.render('partials/todo-list', { todoList, layout: false });
+});
 
 module.exports = router;
